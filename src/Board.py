96ba33d467bc4e73ff562a3e1d98e24d0c2b4ce0
Joey Hagner <jwhagner@gmail.com>
2015-04-10 21:44:12 -0400

    Rewrote place_bomb to pass the test for placing bombs. Also rewrote create_empty_board to prevent the lists from being shallow copied.

--- src/Board.py
+++ src/Board.py
@@ -1,10 +1,13 @@
 class Board:
     @staticmethod
     def create_empty_board(x, y):
-        return [[0] *x] *y
+        new_board = []
+        for i in range(0,y):
+            new_board.append([0] *x)
+        return new_board
 
     def __init__(self, x, y):
         self.board = self.create_empty_board(x, y)
 
     def place_bomb(self, x, y):
-        pass
+        self.board[y][x] = -1
