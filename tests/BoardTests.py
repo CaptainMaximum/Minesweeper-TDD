0d09a8a505f954faa5fb5acf3ceef6a05f4f6523
Joey Hagner <jwhagner@gmail.com>
2015-04-10 21:17:33 -0400

    Wrote a new test for a 3 x 4 board to make Board.create_empty_board fail

--- tests/BoardTests.py
+++ tests/BoardTests.py
@@ -12,5 +12,10 @@ class testCreateEmptyBoard(unittest.TestCase):
         observed_board = Board.create_empty_board(1, 1)
         self.assertEqual(expected_board, observed_board)
 
+    def testDifferentDimensions(self):
+        expected_board = [[0] *3] *4
+        observed_board = Board.create_empty_board(3, 4)
+        self.assertEqual(expected_board, observed_board)
+
 if __name__ == "__main__":
     unittest.main()
