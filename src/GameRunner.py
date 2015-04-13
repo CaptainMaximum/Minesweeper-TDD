61b9dcaf3193b2e97ab17a8062f23093546093d0
Joey Hagner <jwhagner@gmail.com>
2015-04-13 17:39:34 -0400

    Rewrote get_board_params so that it returns a triple of 'None' on failure, so that the function passes the tests that we just rewrote

--- src/GameRunner.py
+++ src/GameRunner.py
@@ -4,12 +4,12 @@ from src.Exceptions import InvalidBoardException
 
 def get_board_params(args=sys.argv):
     if len(args) != 4:
-        return None
+        return (None, None, None)
     arg1, arg2, arg3 = args[1:4]
     try:
         return (int(arg1), int(arg2), int(arg3))
     except ValueError:
-        return None
+        return (None, None, None)
 
 if __name__ == "__main__":
     x, y, bombs = get_board_params()
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         print "Too many bombs: Board size is %d, bombs specified is %d" % (
             e.spaces, e.bombs)
         sys.exit(-1)
-        
+
     victory_status = game.run()
     print game.board
     if victory_status:
