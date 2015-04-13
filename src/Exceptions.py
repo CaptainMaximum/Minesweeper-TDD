bd782a3e112e848ad2159926310f98eb9a6050e1
Joey Hagner <jwhagner@gmail.com>
2015-04-13 17:16:01 -0400

    Added an exception to be thrown from Game.__init__ when the user specifies an invalid board size

---
+++ src/Exceptions.py
@@ -0,0 +1,5 @@
+class InvalidBoardExeption(Exception):
+    '''Raised when the user specifies more bombs than there are spaces'''
+    def __init__(self, spaces, bombs):
+        self.spaces = spaces
+        self.bombs = bombs
