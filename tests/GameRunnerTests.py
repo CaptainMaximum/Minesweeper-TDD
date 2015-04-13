105c0c74ac7b9e58e8e39c8bd5650f46f4dbc229
Joey Hagner <jwhagner@gmail.com>
2015-04-13 17:38:00 -0400

    We need to rewrite get_board_params so that on failure it returns a triple of 'None' I rewrote the tests to reflect this need

--- tests/GameRunnerTests.py
+++ tests/GameRunnerTests.py
@@ -8,17 +8,17 @@ class testGetBoardParams(unittest.TestCase):
         self.assertEqual(expected_value, observed_value)
 
     def testGetBoardParamsTooManyInputs(self):
-        expected_value = None
+        expected_value = (None, None, None)
         observed_value = get_board_params(args=["GameRunner.py", "5", "5", "5", "5"])
         self.assertEqual(expected_value, observed_value)
 
     def testGetBoardParamsInvalidInput(self):
-        expected_value = None
+        expected_value = (None, None, None)
         observed_value = get_board_params(args=["GameRunner.py", "Not", "a", "number"])
         self.assertEqual(expected_value, observed_value)
 
     def testGetBoardParamsTooFewInputs(self):
-        expected_value = None
+        expected_value = (None, None, None)
         observed_value = get_board_params(args=["GameRunner.py"])
         self.assertEqual(expected_value, observed_value)
 
