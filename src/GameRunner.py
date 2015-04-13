e4e0b86f997292980c4f6a725bfcfd973362d15f
Joey Hagner <jwhagner@gmail.com>
2015-04-13 16:40:19 -0400

    Rewrote get_board_params so that it passes testGetBoardParamsValidInput

--- src/GameRunner.py
+++ src/GameRunner.py
@@ -1,7 +1,10 @@
 import sys
 
 def get_board_params(args=sys.argv):
-    pass
+    arg1 = args[1]
+    arg2 = args[2]
+    arg3 = args[3]
+    return (int(arg1), int(arg2), int(arg3))
 
 if __name__ == "__main__":
     x, y, bombs = get_board_params()
