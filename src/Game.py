7dd9c4f0d63e62ff714fefa026af5c5f5cf61f86
Joey Hagner <jwhagner@gmail.com>
2015-04-13 12:48:21 -0400

    Rewrote Game.parse_move so that it passes testInvalidInputToFewArguments

--- src/Game.py
+++ src/Game.py
@@ -27,4 +27,7 @@ class Game:
     def parse_move(self, move_string):
         move_string = move_string.strip("\n\r\t ")
         split_input = re.split("\s+", move_string)
-        return (int(split_input[0]), int(split_input[1]))
+        if len(split_input) < 2:
+            return None
+        else:
+            return (int(split_input[0]), int(split_input[1]))
