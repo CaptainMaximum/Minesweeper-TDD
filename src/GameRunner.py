89c18dc31b4d06f4e996da46a2f16bf74f874363
Joey Hagner <jwhagner@gmail.com>
2015-04-13 17:36:39 -0400

    Added error catching code for if the user specifies to many bombs in GameRunner

--- src/GameRunner.py
+++ src/GameRunner.py
@@ -1,5 +1,6 @@
 import sys
 from src.Game import Game
+from src.Exceptions import InvalidBoardException
 
 def get_board_params(args=sys.argv):
     if len(args) != 4:
@@ -12,7 +13,13 @@ def get_board_params(args=sys.argv):
 
 if __name__ == "__main__":
     x, y, bombs = get_board_params()
-    game = Game(x, y, bombs)
+    try:
+        game = Game(x, y, bombs)
+    except InvalidBoardException as e:
+        print "Too many bombs: Board size is %d, bombs specified is %d" % (
+            e.spaces, e.bombs)
+        sys.exit(-1)
+        
     victory_status = game.run()
     print game.board
     if victory_status:
